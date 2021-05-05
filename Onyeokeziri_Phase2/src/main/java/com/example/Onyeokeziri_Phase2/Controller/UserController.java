d5bcbfa4167e47036b3e0986ea37298dd1048d95
chelseaonyeokeziri@gmail.com <1ibr19sb>
2021-05-04 21:42:23 -0500

    Updatw\w

--- Onyeokeziri_Phase2/src/main/java/com/example/Onyeokeziri_Phase2/Controller/UserController.java
+++ Onyeokeziri_Phase2/src/main/java/com/example/Onyeokeziri_Phase2/Controller/UserController.java
@@ -1,4 +1,39 @@
 package com.example.Onyeokeziri_Phase2.Controller;
 
+import com.example.Onyeokeziri_Phase2.Models.User;
+import com.example.Onyeokeziri_Phase2.Models.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+
+    @Autowired
+    UserRepo userRepo;
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "registration";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "registration_success";
+    }
 }
--- Onyeokeziri_Phase2/src/main/java/com/example/Onyeokeziri_Phase2/Models/UserRepo.java
+++ Onyeokeziri_Phase2/src/main/java/com/example/Onyeokeziri_Phase2/Models/UserRepo.java
@@ -1,8 +1,11 @@
 package com.example.Onyeokeziri_Phase2.Models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User,Long> {
+    @Query("SELECT u FROM User u WHERE u.Email = ?1")
+    public User findByEmail(String Email);
 }
